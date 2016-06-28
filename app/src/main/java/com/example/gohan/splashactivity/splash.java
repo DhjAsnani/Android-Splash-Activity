1b3f981efd021a425b5199848bb0158fa157b469
dhjasnani <asnani.dheeraj@yahoo.com>
2016-06-29 01:33:35 +0530

    Animation declared inside splash

--- app/src/main/java/com/example/gohan/splashactivity/splash.java
+++ app/src/main/java/com/example/gohan/splashactivity/splash.java
@@ -2,6 +2,9 @@ package com.example.gohan.splashactivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 /**
  * Created by Freeware Sys on 29-Jun-16.
@@ -11,5 +14,11 @@ public class splash extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashh);
+        final ImageView iv = (ImageView) findViewById(R.id.imageView);
+        // declaring animations
+        final Animation animation = AnimationUtils.loadAnimation(getBaseContext(),R.anim.rotate);
+
+        // start animation
+        
     }
 }
