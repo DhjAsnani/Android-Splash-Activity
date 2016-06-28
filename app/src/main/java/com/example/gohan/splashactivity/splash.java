c605bfa112eeefe0fcd3eeb56517305638c62711
dhjasnani <asnani.dheeraj@yahoo.com>
2016-06-29 01:11:36 +0530

    Splash Acivity first commit

---
+++ app/src/main/java/com/example/gohan/splashactivity/splash.java
@@ -0,0 +1,14 @@
+package com.example.gohan.splashactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Freeware Sys on 29-Jun-16.
+ */
+public class splash extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
