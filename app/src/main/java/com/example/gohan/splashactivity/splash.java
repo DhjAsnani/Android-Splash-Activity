2633b7cdfcd2a07c7d9890336a7e8faa4d1abad3
dhjasnani <asnani.dheeraj@yahoo.com>
2016-06-29 01:14:34 +0530

    Splash Acivity Layout and class for splash created

--- app/src/main/java/com/example/gohan/splashactivity/splash.java
+++ app/src/main/java/com/example/gohan/splashactivity/splash.java
@@ -10,5 +10,6 @@ public class splash extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashh);
     }
 }
