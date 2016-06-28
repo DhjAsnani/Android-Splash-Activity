84586a7f3ce56a890f302c71c12f062c0537871d
dhjasnani <asnani.dheeraj@yahoo.com>
2016-06-29 01:48:18 +0530

    Animation Implemented

--- app/src/main/java/com/example/gohan/splashactivity/splash.java
+++ app/src/main/java/com/example/gohan/splashactivity/splash.java
@@ -1,6 +1,7 @@
 package com.example.gohan.splashactivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -17,8 +18,29 @@ public class splash extends Activity {
         final ImageView iv = (ImageView) findViewById(R.id.imageView);
         // declaring animations
         final Animation animation = AnimationUtils.loadAnimation(getBaseContext(),R.anim.rotate);
+        // fadeout
+        final Animation animation2 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.abc_fade_out);
 
         // start animation
-        
+        iv.startAnimation(animation);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                iv.startAnimation(animation2);
+                finish();
+                Intent i = new Intent(splash.this,MainActivity.class);
+                startActivity(i);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 }
